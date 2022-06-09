ee75a996c77918a71e82caca8d586d065e9a7a1e
caipaofan <954698621@qq.com>
2022-06-09 20:14:46 +0800

    完成了首页制作和退出功能

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,3 +1,10 @@
 $.ajaxPrefilter((options)=>{
     options.url = 'http://www.liulongbin.top:3007'+options.url
+
+    if (options.url.includes('/my/')) {
+        options.headers={
+            Authorization: localStorage.getItem('token')
+        }
+    }
 })
+
---
+++ assets/js/index.js
@@ -0,0 +1,45 @@
+function getUserInfo() {
+    $.ajax({
+        method: 'GET',
+        url:'/my/userinfo',
+        // headers: {
+        //     Authorization: localStorage.getItem('token')
+        // },
+        success: (res) => {
+            console.log(res);
+            if (res.status!==0) {
+                return layer.msg(res.message);
+            }
+            layer.msg('获取用户信息成功')
+            renderAvatar(res.data)
+        }
+    })
+}
+
+const renderAvatar= (user) => {
+    const name =user.nickname || user.username
+    $('#welcome').html(`欢迎 ${name}`)
+    if (user.user_pic !==null) {
+        $('.layui-nav-img').attr('src',user_pic).show()
+        $('.text-avatar').hide()
+    }else {
+        $('.layui-nav-img').hide()
+        let first =name[0].toUpperCase()
+        $('.text-avatar').html(first).show()
+    }
+
+
+}
+
+$('#btnLogout').click(()=>{
+    layer.confirm('Are you sure you want 退出？',{
+        icon: 3, title: "提示"
+    },function(index){
+        localStorage.removeItem('token')
+        location.href='/login.html'
+    })
+})
+
+
+
+getUserInfo()
