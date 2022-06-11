a1f05143f968e99aefb49cf3014c1eba0e2d4c03
caipaofan <954698621@qq.com>
2022-06-11 10:02:35 +0800

    index完成了首页

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -6,5 +6,14 @@ $.ajaxPrefilter((options)=>{
             Authorization: localStorage.getItem('token')
         }
     }
+
+    options.complete=(res)=>{
+        if(res.responseJSON.status ===1 && res.responseJSON.message === "身份认证失败！") {
+            //  强制清空 token
+            localStorage.removeItem("token");
+            // 强制跳转到登录页面
+            location.href = "/login.html"
+        }
+    }
 })
 
